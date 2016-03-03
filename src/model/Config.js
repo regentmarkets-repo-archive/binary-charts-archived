c503e0a37aa08d9d4c66957261f5840787d84272
qingwei <qingwei@binary.com>
2016-03-03 14:32:35 +0800

    Bump version for publish

--- src/model/Config.js
+++ src/model/Config.js
@@ -1,5 +1,29 @@
-/**
- * Organize by having a default configs
- * then extend it for each type of charts
- * then expose functions to override default values
- */
+const seriesConfig = (a = {
+    width: width = 2,
+    color: color = 'green',
+    areaColor: areaColor = 'blue',
+    areaOpacity: areaOpacity = 0.2,
+    labelColor: labelColor = 'red',
+    labelFontSize: labelFontSize = 12,
+    labelTextColor: labelTextColor = 'white'
+} = {}) => a;
+
+const RiseFallConfig = {
+    mainSeries: seriesConfig({
+        width: 2,
+        color: 'green',
+        areaColor: 'blue',
+    }),
+    contractSeries: seriesConfig({
+        width: 2,
+        color: 'orange',
+        areaColor: 'orange',
+        areaOpacity: 0.2,
+
+    }),
+    currentSpotSeries: seriesConfig({
+        width: 2,
+        color: 'orange'
+    }),
+
+};
