dcdf48488853ad6fb7336bbe6814cb77bf1eef69
Boris Yankov <borisyankov@gmail.com>
2016-03-15 15:54:36 +0800

    Handle unknown trade types

--- src/configurator/plotBandsForContractAndTrade.js
+++ src/configurator/plotBandsForContractAndTrade.js
@@ -3,5 +3,7 @@ import * as confs from '../plot-bands/';
 export default (contractOrTrade, lastTick) => {
     if (!contractOrTrade) return [];
     const type = contractOrTrade.type || contractOrTrade.contract_type;
-    return confs[type.toLowerCase() + 'PlotBand'](contractOrTrade, lastTick);
+    const configFunc = confs[type.toLowerCase() + 'PlotBand'];
+    if (!configFunc) throw Error('Not a known type: ' + type);
+    return configFunc(contractOrTrade, lastTick);
 }
