5381f1447efa327a76f6136e8a3318149a98e121
Boris Yankov <borisyankov@gmail.com>
2016-03-20 23:19:35 +0800

    Add upOrDown and range plot bands

--- src/plot-bands/expiryMissPlotBand.js
+++ src/plot-bands/expiryMissPlotBand.js
@@ -1,7 +1,2 @@
-import { winPlotBand, lossPlotBand } from './_commonPlotBands';
-
-export default contract =>  [
-    winPlotBand(0, contract.barrier),
-    lossPlotBand(contract.barrier, contract.barrier2),
-    winPlotBand(contract.barrier2, Number.MAX_VALUE),
-];
+import upOrDownPlotBand from './upOrDownPlotBand';
+export default upOrDownPlotBand;
--- src/plot-bands/expiryRangePlotBand.js
+++ src/plot-bands/expiryRangePlotBand.js
@@ -1,7 +1,2 @@
-import { winPlotBand, lossPlotBand } from './_commonPlotBands';
-
-export default contract =>  [
-    lossPlotBand(0, contract.barrier),
-    winPlotBand(contract.barrier, contract.barrier2),
-    lossPlotBand(contract.barrier2, Number.MAX_VALUE),
-];
+import rangePlotBand from './rangePlotBand';
+export default rangePlotBand;
