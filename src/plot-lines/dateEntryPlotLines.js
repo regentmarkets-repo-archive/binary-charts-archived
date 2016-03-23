2c09bc5a99b9c088d1fa8abb18da30c0a238a023
Boris Yankov <borisyankov@gmail.com>
2016-03-23 14:44:44 +0200

    Add plot lines for more time points and fix overlapping text for some that fall on the same time

--- src/plot-lines/dateEntryPlotLines.js
+++ src/plot-lines/dateEntryPlotLines.js
@@ -1,10 +1,11 @@
-const vertPlotLine = (epoch, color, text) => ({
+const vertPlotLine = (epoch, color, text, textAlign) => ({
     id: text,
     value: epoch * 1000,
     color,
     width: 2,
     label: {
         text,
+        textAlign,
         style: {
             color,
         }
@@ -12,9 +13,13 @@ const vertPlotLine = (epoch, color, text) => ({
 });
 
 export default (contract) => [
-    vertPlotLine(contract.date_expiry, 'navy', 'Time of Expiry'),
-    vertPlotLine(contract.date_settlement, 'navy', 'Settlement Time'),
-    vertPlotLine(contract.date_start, 'navy', 'Start Time'),
-    vertPlotLine(contract.entry_tick_time, 'navy', 'Entry Spot'),
-    vertPlotLine(contract.expiry_time, 'navy', 'Expiry Time'),
+    vertPlotLine(contract.purchase_time, 'navy', 'Purchase Time', 'left'),
+    vertPlotLine(contract.date_start, 'navy', 'Start Time', 'left'),
+    vertPlotLine(contract.entry_tick_time, 'navy', 'Entry Spot', 'right'),
+    vertPlotLine(contract.date_expiry, 'navy', 'Time of Expiry', 'left'),
+    vertPlotLine(contract.date_settlement, 'navy', 'Settlement Time', 'left'),
+    vertPlotLine(contract.expiry_time, 'navy', 'End Time', 'right'),
+    vertPlotLine(contract.exit_tick_time, 'navy', 'Exit Spot', 'left'),
+    vertPlotLine(contract.sell_spot_time, 'navy', 'Sell Time', 'right'),
+
 ];
