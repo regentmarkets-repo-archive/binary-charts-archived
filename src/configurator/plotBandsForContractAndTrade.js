722a0aa16ce8b9ac97acf313772e216c9d132b13
qingwei <qingwei@binary.com>
2016-03-31 13:08:07 +0800

    Do not fail if no contract_type

--- src/configurator/plotBandsForContractAndTrade.js
+++ src/configurator/plotBandsForContractAndTrade.js
@@ -1,9 +1,10 @@
 import * as confs from '../plot-bands/';
 
 export default (contractOrTrade, lastTick) => {
-    if (!contractOrTrade) return [];
-
     const type = contractOrTrade.type || contractOrTrade.contract_type;
+
+    if (!contractOrTrade || !type) return [];
+    
     const configFunc = confs[type.toLowerCase() + 'PlotBand'];
 
     if (!configFunc) throw Error('Not a known type: ' + type);
