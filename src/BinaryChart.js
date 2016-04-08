1bf2ae2c4af4e44f5edcfd95a1f4e6ab405e4751
Boris Yankov <borisyankov@gmail.com>
2016-04-08 20:30:08 +0300

    Refactor types into BinaryTypes

--- src/BinaryChart.js
+++ src/BinaryChart.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import shallowEqual from 'fbjs/lib/shallowEqual';
 import ReactHighstock from 'react-highcharts/bundle/ReactHighstock.src';
+import * as BinaryTypes from './BinaryTypes';
 import { areTickArraysEqual, doTicksDifferJustOneEntry, tickToData } from './_utils';
 import { fullConfig, updateChart } from './configurator';
 import { getLastTick } from './_utils';
@@ -17,41 +18,13 @@ if (Object.keys(ReactHighstock.Highcharts).length > 0) {
     ReactHighstock.Highcharts.setOptions(theme);
 }
 
-const barrierType = PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-]);
-
-const contractOrTradeShape = PropTypes.shape({
-    barrier: barrierType,
-    barrier2: barrierType,
-    contract_type: PropTypes.string.isRequired,
-    date_expiry: PropTypes.number,
-    date_settlement: PropTypes.number,
-    date_start: PropTypes.number,
-    entry_spot: PropTypes.number,
-    entry_tick_time: PropTypes.number,
-    exit_tick_time: PropTypes.number,
-    expiry_time: PropTypes.number,
-    purchase_time: PropTypes.number,
-    sell_spot_time: PropTypes.number,
-});
-
-const tickArrayType = PropTypes.arrayOf(PropTypes.shape({
-    epoch: PropTypes.number.isRequired,
-    quote: PropTypes.number.isRequired,
-}));
-
 export default class BinaryChart extends Component {
 
     static propTypes = {
         symbol: PropTypes.string,
-        ticks: PropTypes.oneOfType([
-            tickArrayType,
-            PropTypes.function,
-        ]).isRequired,
-        contract: contractOrTradeShape,
-        trade: contractOrTradeShape,
+        ticks: BinaryTypes.ticksType,
+        contract: BinaryTypes.contractOrTradeShape,
+        trade: BinaryTypes.contractOrTradeShape,
     };
 
     static defaultProps = {
---
+++ src/BinaryTypes.js
@@ -0,0 +1,31 @@
+import { PropTypes } from 'react';
+
+export const barrierType = PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+]);
+
+export const contractOrTradeShape = PropTypes.shape({
+    barrier: barrierType,
+    barrier2: barrierType,
+    contract_type: PropTypes.string.isRequired,
+    date_expiry: PropTypes.number,
+    date_settlement: PropTypes.number,
+    date_start: PropTypes.number,
+    entry_spot: PropTypes.number,
+    entry_tick_time: PropTypes.number,
+    exit_tick_time: PropTypes.number,
+    expiry_time: PropTypes.number,
+    purchase_time: PropTypes.number,
+    sell_spot_time: PropTypes.number,
+});
+
+export const tickArrayType = PropTypes.arrayOf(PropTypes.shape({
+    epoch: PropTypes.number.isRequired,
+    quote: PropTypes.number.isRequired,
+}));
+
+export const ticksType = PropTypes.oneOfType([
+    tickArrayType,
+    PropTypes.function,
+]);
