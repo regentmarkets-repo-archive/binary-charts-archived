883455a147425843dab7dc12a2cbc016e08a7b7c
Boris Yankov <borisyankov@gmail.com>
2016-04-10 15:12:19 +0300

    Fix tests

--- src/config/__tests__/strTimePlusDayAsEpoch-test.js
+++ src/config/__tests__/strTimePlusDayAsEpoch-test.js
@@ -3,20 +3,20 @@ import { strTimePlusDayAsEpoch } from '../updateTradingTimes';
 
 describe('strTimePlusDayAsEpoch', () => {
     it('same if 00', () => {
-        const day = new Date('2020-1-1');
+        const day = new Date(2020, 1, 1);
         const epoch = strTimePlusDayAsEpoch(day, '00:00:00');
-        expect(1577829600).to.equal(epoch);
+        expect(1580508000).to.equal(epoch);
     });
 
     it('string is 24hr', () => {
-        const day = new Date('2020-1-1');
+        const day = new Date(2020, 1, 1);
         const epoch = strTimePlusDayAsEpoch(day, '15:10:00');
-        expect(1577851815).to.equal(epoch);
+        expect(1580530215).to.equal(epoch);
     });
 
     it('max is 23', () => {
-        const day = new Date('2020-1-1');
+        const day = new Date(2020, 1, 1);
         const epoch = strTimePlusDayAsEpoch(day, '23:59:59');
-        expect(1577866283).to.equal(epoch);
+        expect(1580544683).to.equal(epoch);
     });
 });
