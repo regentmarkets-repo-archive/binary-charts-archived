a598ed7c5d60c5e50c7074350ad4e38f31a37d5e
Boris Yankov <borisyankov@gmail.com>
2016-04-28 22:21:28 +0300

    Simplification of data retreival code

--- .storybook/data-retrieval.js
+++ .storybook/data-retrieval.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@kadira/storybook';
 import BinaryChart from '../src/BinaryChart';
 import { LiveApi } from 'binary-live-api';
 
-const token = 'qdJ86Avvrsh0Le4'
+const token = 'DKTDX5MnzTPTM43';
 const api = new LiveApi();
 
 class DynamicChart extends React.Component {
@@ -15,36 +15,39 @@ class DynamicChart extends React.Component {
     }
 
     componentWillMount() {
-        api.authorize(token).then(() => {
-            return api.getStatement({ description: 1, limit: 1 });
-        }).then(r => {
-            const contractID = r.statement.transactions[0].contract_id;
-            return api.subscribeToOpenContract(contractID);
-        }).then(r => {
+        api.authorize(token).then(() =>
+            api.getStatement({ description: 1, limit: 1 })
+        ).then(r =>
+            api.subscribeToOpenContract(r.statement.transactions[0].contract_id)
+        ).then(r => {
             const contract = r.proposal_open_contract;
             const symbol = contract.underlying;
             const purchaseT = contract.purchase_time;
             const sellT = contract.sell_time;
             this.setState({ contract });
-            return api.getTickHistory(symbol, { start: purchaseT, end: sellT, adjust_start_time: 1 });
+            return api.getTickHistory(symbol, {
+                start: purchaseT,
+                end: sellT,
+                adjust_start_time: 1,
+            });
         }).then(r => {
             const ticks = r.history.times.map((t, idx) => {
                 const quote = r.history.prices[idx];
                 return { epoch: +t, quote: +quote };
             });
             this.setState({ ticks });
-        })
+        });
     }
 
     render() {
         const { ticks, contract } = this.state;
         return (
             <BinaryChart ticks={ticks} contract={contract} />
-        )
+        );
     }
 }
 
 storiesOf('Data retrieval', module)
-    .add('rise fall', () => {
-        return <DynamicChart />
-    });
+    .add('rise fall', () =>
+        <DynamicChart />
+    );
--- .storybook/past-contracts.js
+++ .storybook/past-contracts.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import DynamicChart from './DynamicChart';
+import { LiveApi } from 'binary-live-api';
+
+const token = 'DKTDX5MnzTPTM43';
+const api = new LiveApi();
+
+const getData = (contractId) => {
+    api.authorize(token).then(
+        api.subscribeToOpenContract(contractId)
+    ).then(r => {
+        const contract = r.proposal_open_contract;
+        const symbol = contract.underlying;
+        this.setState({ contract });
+        return api.getTickHistory(symbol, {
+            start: contract.purchase_time,
+            end: contract.sell_time,
+            adjust_start_time: 1,
+        });
+    }).then(r => {
+        const ticks = r.history.times.map((t, idx) => ({
+            epoch: +t,
+            quote: +r.history.prices[idx],
+        }));
+        this.setState({ ticks });
+    });
+};
 
 storiesOf('Past Contracts', module)
-    .add('Empty', () =>
-        <DynamicChart />
+    .add('1st', () =>
+        <BinaryChart ticks={ticks} contract={contract} />
     );
