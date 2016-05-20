8bd1658aeb5f9a1c4187aa9ba2f7ae55ee50c1af
Boris Yankov <borisyankov@gmail.com>
2016-05-20 06:23:57 +0300

    Reenable time plot lines

--- src/plot-lines/timePlotLines.js
+++ src/plot-lines/timePlotLines.js
@@ -1,31 +1,27 @@
 // entries are in chronological order
 export default [
-    // { id: 'date_start', name: 'Start Time', position: 'left' },
-    // Start time - ??? ***
+    { id: 'date_start', position: 'left' },
+    // St`art Time
 
-    // { id: 'purchase_time', name: 'Purchase Time', position: 'right' },
+    { id: 'purchase_time', position: 'right' },
     // Time at which contract was purchased, present only for sell transaction
 
-    { id: 'entry_tick_time', name: 'Entry Spot', position: 'right' },
-    // Entry spot - ??? ***
+    { id: 'entry_tick_time', position: 'right' },
+    // Entry Spot Time
 
-    // { id: 'date_expiry', name: 'End Time', position: 'left' },
+    { id: 'date_expiry', position: 'left' },
     // Time when contract is expected to expire
-    // { id: 'expiry_time', name: 'End Time', position: 'left' },
+    // { id: 'expiry_time', position: 'left' },
     // Expiry time is the same as date_expiry but incorrectly named in Portfolio call
 
     { id: 'exit_tick_time', name: 'Exit Spot', position: 'left' },
     // Time when the contact actually expires
-    // Do not show if value is close to date_expiry
+    // Do not show if value is same as date_expiry
 
-    // { id: 'date_settlement', name: 'Settlement Time', position: 'right' },
+    { id: 'date_settlement', position: 'right' },
     // Settlement time, depends on market, shown in trading times
 
-    // { id: 'sell_spot_time', name: 'Sell Time', position: 'left' },
-    // Time of tick when contract are sold
-    // If there isn’t any tick on that point, it will use the previous tick
-
-    // { id: 'sell_time', name: 'Server Sell Time', position: 'right' },
+    { id: 'sell_time', position: 'right' },
     // Time when server performed sell action
 ];
 
