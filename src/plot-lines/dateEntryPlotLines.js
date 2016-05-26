8ab422206c22527caaae9162ed72b19d61ea12a9
qingwei <qingwei@binary.com>
2016-05-26 16:42:00 +0800

    Reduce line to show

--- src/plot-lines/dateEntryPlotLines.js
+++ src/plot-lines/dateEntryPlotLines.js
@@ -22,7 +22,8 @@ export default (contract) => {
 
     return timePlotLines
         .filter(param => contract[param.id])
-        .filter(param => param.id !== 'purchase_time' || shoudShowPurchaseTime(contract))
+        .filter(param => param.id !== 'purchase_time')
+        .filter(param => param.id !== 'date_expiry')
         // .filter(param => param.id !== 'exit_tick_time' || shouldShowExitSpot(contract))
         .filter(param => param.id !== 'date_settlement' || shouldShowSettlement(contract))
         .map(param =>
