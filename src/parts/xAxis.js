6dfd214596e8939069817e891f6d2b3ab6730d6f
qingwei <qingwei@binary.com>
2016-06-01 13:18:05 +0800

    Fix test

--- src/parts/xAxis.js
+++ src/parts/xAxis.js
@@ -8,7 +8,7 @@ export default ({ rangeChange = () => undefined }) => ({
     startOnTick: false,
     endOnTick: false,
     events: {
-        setExtremes: function(e) {
+        setExtremes: function setExtremesHandler(e) {          // eslint-disable-line object-shorthand
             if (e.rangeSelectorButton) {
                 const chart = this.chart;
 
@@ -24,7 +24,7 @@ export default ({ rangeChange = () => undefined }) => ({
                 // a hack so that we can set x-extremes correctly after data is loaded
                 // works best if rangechange is only fire when needed.
                 if (asyncResult.then) {
-                    const buttonID = buttons.findIndex(button => button.text == text);
+                    const buttonID = buttons.findIndex(button => button.text === text);
                     asyncResult.then(() => {
                         chart.binary.rangeSelectedProgrammatically = true;
                         chart.rangeSelector.clickButton(buttonID, e.rangeSelectorButton, true);
@@ -32,7 +32,7 @@ export default ({ rangeChange = () => undefined }) => ({
                 }
             }
         },
-        afterSetExtremes: function handler() { // eslint-disable-line object-shorthand
+        afterSetExtremes: function afterSetExtremesHandler() { // eslint-disable-line object-shorthand
             const chart = this.chart;
             if (!chart.binary) {
                 return;
