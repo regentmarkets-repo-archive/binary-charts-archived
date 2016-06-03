b20add407898a874cca40956a10738be5a71c233
qingwei <qingwei@binary.com>
2016-06-03 17:13:27 +0800

    Handle different chart type better

--- src/config/updateTicks.js
+++ src/config/updateTicks.js
@@ -1,37 +1,47 @@
 import doTicksDifferJustOneEntry from 'binary-utils/lib/doTicksDifferJustOneEntry';
+import doCandlesDifferJustOneEntry from 'binary-utils/lib/doCandlesDifferJustOneEntry';
 import tickToData from 'binary-utils/lib/tickToData';
 import ohlcToData from 'binary-utils/lib/ohlcToData';
 import getLastTick from 'binary-utils/lib/getLastTick';
 
 export default (chart, prevProps, nextProps) => {
-    const oneTickDiff = doTicksDifferJustOneEntry(prevProps.ticks, nextProps.ticks);
-    if (oneTickDiff) {
-        const lastTick = getLastTick(nextProps.ticks);
-        const chartType = chart.series[0].type;
-
-        if (chartType === 'area') {
-            const dataPoint = tickToData(lastTick);
-            chart.series[0].addPoint(dataPoint, false);
-        } else if (chartType === 'candlestick') {
-            const dataPoint = ohlcToData(lastTick);
-            const xData = chart.series[0].xData;
-            const last2Epoch = xData[xData.length - 2];
-            const last3Epoch = xData[xData.length - 3];
-            const timeInterval = last2Epoch - last3Epoch;
-
-            const newDataIsWithinInterval = (dataPoint[0] - last2Epoch) <= timeInterval;
-            if (newDataIsWithinInterval) {
-                chart.series[0].options.data[xData.length - 1] = dataPoint;
-            } else {
+    const chartType = chart.series[0].type;
+    switch (chartType) {
+        case 'area': {
+            const oneTickDiff = doTicksDifferJustOneEntry(prevProps.ticks, nextProps.ticks);
+            if (oneTickDiff) {
+                const lastTick = getLastTick(nextProps.ticks);
+                const dataPoint = tickToData(lastTick);
                 chart.series[0].addPoint(dataPoint, false);
+            } else {
+                const dataList = nextProps.ticks.map(tickToData);
+                chart.series[0].setData(dataList, false);
             }
+            break;
         }
-    } else {
-        const dataList =
-            chart.series[0].type === 'area' ?
-                nextProps.ticks.map(tickToData) :
-                nextProps.ticks.map(ohlcToData);
-        chart.series[0].setData(dataList, false);
+        case 'candlestick': {
+            const oneTickDiff = doCandlesDifferJustOneEntry(prevProps.ticks, nextProps.ticks);
+            if (oneTickDiff) {
+                const lastTick = getLastTick(nextProps.ticks);
+                const dataPoint = ohlcToData(lastTick);
+                const xData = chart.series[0].xData;
+                const last2Epoch = xData[xData.length - 2];
+                const last3Epoch = xData[xData.length - 3];
+                const timeInterval = last2Epoch - last3Epoch;
+
+                const newDataIsWithinInterval = (dataPoint[0] - last2Epoch) <= timeInterval;
+                if (newDataIsWithinInterval) {
+                    chart.series[0].options.data[xData.length - 1] = dataPoint;
+                } else {
+                    chart.series[0].addPoint(dataPoint, false);
+                }
+            } else {
+                const dataList = nextProps.ticks.map(ohlcToData);
+                chart.series[0].setData(dataList, false);
+            }
+            break;
+        }
+        default: console.warn('Unexpected highchart series type: ', chartType);
     }
     chart.binary.ticks = nextProps.ticks;
 };
