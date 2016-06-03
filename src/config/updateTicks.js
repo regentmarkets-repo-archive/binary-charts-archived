e1a9241aa61463b5fbd10020dc3037869b85b479
qingwei <qingwei@binary.com>
2016-06-03 11:45:48 +0800

    Support streaming

--- src/config/updateTicks.js
+++ src/config/updateTicks.js
@@ -7,8 +7,25 @@ export default (chart, prevProps, nextProps) => {
     const oneTickDiff = doTicksDifferJustOneEntry(prevProps.ticks, nextProps.ticks);
     if (oneTickDiff) {
         const lastTick = getLastTick(nextProps.ticks);
-        const dataPoint = chart.series[0].type === 'area' ? tickToData(lastTick) : ohlcToData(lastTick);
-        chart.series[0].addPoint(dataPoint, false);
+        const chartType = chart.series[0].type;
+
+        if (chartType === 'area') {
+            const dataPoint = tickToData(lastTick);
+            chart.series[0].addPoint(dataPoint, false);
+        } else if (chartType === 'candlestick') {
+            const dataPoint = ohlcToData(lastTick);
+            const xData = chart.series[0].xData;
+            const last2Epoch = xData[xData.length - 2];
+            const last3Epoch = xData[xData.length - 3];
+            const timeInterval = last2Epoch - last3Epoch;
+
+            const newDataIsWithinInterval = (dataPoint[0] - last2Epoch) <= timeInterval;
+            if (newDataIsWithinInterval) {
+                chart.series[0].options.data[xData.length - 1] = dataPoint;
+            } else {
+                chart.series[0].addPoint(dataPoint, false);
+            }
+        }
     } else {
         const dataList =
             chart.series[0].type === 'area' ?
