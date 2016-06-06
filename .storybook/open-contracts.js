e38d23e0e7dcc45f567ce4573ee9ce8912f50153
qingwei <qingwei@binary.com>
2016-06-06 11:04:00 +0800

    Update contract for testing

--- .storybook/open-contracts.js
+++ .storybook/open-contracts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import ContractChart from './charts/ContractChart';
 
-const contract5day = '8686428788';
+const contract5day = '9009573328';
 
 storiesOf('Open Contracts', module)
     .add('5 days contract', () =>
