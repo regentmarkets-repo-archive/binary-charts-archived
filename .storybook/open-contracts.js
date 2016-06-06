8b936479f6095a53c826642b5927c65272acc59f
qingwei <qingwei@binary.com>
2016-06-06 13:18:52 +0800

    Change to not yet started contract

--- .storybook/open-contracts.js
+++ .storybook/open-contracts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import ContractChart from './charts/ContractChart';
 
-const contract5day = '9009573328';
+const contract5day = '9068873548';
 
 storiesOf('Open Contracts', module)
     .add('5 days contract', () =>
