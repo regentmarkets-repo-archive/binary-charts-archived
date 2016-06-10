aec4f3a709f723ba84d957e5af7f727784ed69ac
qingwei <qingwei@binary.com>
2016-06-10 13:12:29 +0800

    Fix lint

--- src/plugins/addLoadingFlag.js
+++ src/plugins/addLoadingFlag.js
@@ -1,13 +1,13 @@
 import { wrap, Chart } from 'highcharts/highstock';
 
 export default () => {
-    wrap(Chart.prototype, 'showLoading', function(proceed, ...args) {
+    wrap(Chart.prototype, 'showLoading', function showLoading(proceed, ...args) {
         this.isLoading = true;
         proceed.apply(this, args);
     });
 
-    wrap(Chart.prototype, 'hideLoading', function(proceed, ...args) {
+    wrap(Chart.prototype, 'hideLoading', function hideLoading(proceed, ...args) {
         this.isLoading = false;
         proceed.apply(this, args);
     });
-}
+};
