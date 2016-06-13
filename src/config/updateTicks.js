f02f00de7b9d4d3fc99495995edbd458c2e45198
qingwei <qingwei@binary.com>
2016-06-13 10:00:51 +0800

    Do not redraw in the middle

--- src/config/updateTicks.js
+++ src/config/updateTicks.js
@@ -20,7 +20,7 @@ export default (chart, prevProps, nextProps) => {
                 }
             } else {
                 const dataList = nextProps.ticks.map(tickToData);
-                chart.series[0].setData(dataList);
+                chart.series[0].setData(dataList, false);
                 if (dataList[dataList.length - 1]) {
                     newDataMax = dataList[dataList.length - 1][0];
                 }
@@ -51,7 +51,7 @@ export default (chart, prevProps, nextProps) => {
                 }
             } else {
                 const dataList = nextProps.ticks.map(ohlcToData);
-                chart.series[0].setData(dataList);
+                chart.series[0].setData(dataList, false);
             }
             break;
         }
