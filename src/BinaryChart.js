dd7db369611a64a20286a71b4eed92ed01675a26
qingwei <qingwei@binary.com>
2016-08-03 11:31:08 +0800

    Support dynamic noData changes

--- src/BinaryChart.js
+++ src/BinaryChart.js
@@ -98,7 +98,8 @@ export default class BinaryChart extends Component {
     shouldComponentUpdate(nextProps) {
         if (
             this.props.symbol !== nextProps.symbol ||
-                this.props.type !== nextProps.type                 // this might break when we have more chart type
+                this.props.type !== nextProps.type ||                // this might break when we have more chart type
+                this.props.noData !== nextProps.noData
         ) {
             this.destroyChart();
             this.createChart(nextProps);
