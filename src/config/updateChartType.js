885efd0ae8fca4ee1967c99db57d2d692841264e
qingwei <qingwei@binary.com>
2016-09-06 17:28:04 +0800

    Prevent unnecessary redraw

--- src/config/updateChartType.js
+++ src/config/updateChartType.js
@@ -2,24 +2,25 @@ import chartTypeToDataType from '../utils/chartTypeToDataType';
 import getSeriesByType from '../utils/getSeriesByType';
 
 export default (chart: Chart, newType) => {
-    const dataType = chartTypeToDataType(chartType);
+    const dataType = chartTypeToDataType(newType);
     const mainTickSeries = getSeriesByType(chart, 'line');
     const mainOhlcSeries = getSeriesByType(chart, 'ohlc');
+
     switch (dataType) {
         case 'tick':
             if (mainOhlcSeries) mainOhlcSeries.hide();
             if (mainTickSeries) {
                 mainTickSeries.show();
-                mainTickSeries.update({ type: newType });
+                mainTickSeries.update({ type: newType }, false);
             }
             break;
         case 'ohlc':
             if (mainTickSeries) mainTickSeries.hide();
             if (mainOhlcSeries) {
                 mainOhlcSeries.show();
-                mainOhlcSeries.update({ type: newType });
+                mainOhlcSeries.update({ type: newType }, false);
             }
             break;
         default: throw new Error(`Unknown data type: ${dataType}`);
     }
-}
+};
