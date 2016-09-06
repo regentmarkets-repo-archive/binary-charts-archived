f253f3a519e44215ec7143ab58ec2b8e37b1d86c
qingwei <qingwei@binary.com>
2016-09-06 12:29:42 +0800

    Add id to series

--- src/parts/seriesLine.js
+++ src/parts/seriesLine.js
@@ -1,7 +1,19 @@
+const chartTypeToDataType = (type: ChartType) => {
+    switch (type) {
+        case 'area':
+        case 'line':
+            return 'tick';
+        case 'ohlc':
+        case 'candlestick':
+            return 'ohlc';
+    }
+};
+
 export default (data: (ChartTick | ChartCandle)[], pipSize: number, type: ChartType) => [{
     name: 'Spot',
     type,
     data,
+    id: `main-${chartTypeToDataType(type)}`,
     tooltip: {
         valueDecimals: pipSize,
         zIndex: 120,
