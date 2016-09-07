6020462a0bd0d8a822151356ab5ed9a12ed30cab
Qing Wei <qingwei@binary.com>
2016-09-07 16:55:43 +0800

    Qingwei/interval (#124)
    * Disable interval picker when showing tick chart
    
    * Add interval change handler
    
    * Remove type, not needed

--- src/toolbar/IntervalPicker.js
+++ src/toolbar/IntervalPicker.js
@@ -1,25 +1,23 @@
 import React, { PureComponent } from 'react';
 
 type Props = {
+    onChange: (interval: ChartInterval) => void,
 };
 
 export default class IntervalPicker extends PureComponent {
 
     props: Props;
 
-    static defaultProps = {
-    };
-
     render() {
+        const { onChange } = this.props;
         return (
-            <select>
-                <option value="ticks">Ticks</option>
-                <option value="1m">1 minute</option>
-                <option value="5m">5 minutes</option>
-                <option value="15m">15 minutes</option>
-                <option value="1h">1 hour</option>
-                <option value="4h">4 hours</option>
-                <option value="1d">1 day</option>
+            <select onChange={e => onChange(e.target.value)}>
+                <option value={60}>1 minute</option>
+                <option value={300}>5 minutes</option>
+                <option value={15 * 60}>15 minutes</option>
+                <option value={60 * 60}>1 hour</option>
+                <option value={4 * 60 * 60}>4 hours</option>
+                <option value={24 * 60 * 60}>1 day</option>
             </select>
         );
     }
