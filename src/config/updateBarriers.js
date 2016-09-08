bd19718d378ce08e6844dc1eb4e0246dcf138474
qingwei <qingwei@binary.com>
2016-09-08 15:18:18 +0800

    Fix barrier, barrier series need to be line that span through all data

--- src/config/updateBarriers.js
+++ src/config/updateBarriers.js
@@ -9,6 +9,7 @@ const extractBarrierLine = (chart, contract) => {
     const currentSpot = lastPriceFromSeries(mainSeries);
 
     const { dataMax } = chart.xAxis[0].getExtremes();
+    const minData = mainSeries.xData[0];
 
     return barrierIds.map(b => {
         const hasBarrier = contract && contract[b] &&
@@ -20,7 +21,7 @@ const extractBarrierLine = (chart, contract) => {
         }
 
         const yVal = +contract[b];
-        const data = [[dataMax, yVal]];
+        const data = [[minData, yVal], [dataMax, yVal]];
 
         return seriesLine(data, pipSize, 'line', b);
     });
