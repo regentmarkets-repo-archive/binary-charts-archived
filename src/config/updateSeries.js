2c91f287f40bd21797aaba332d4b80d2a7cdbce9
qingwei <qingwei@binary.com>
2016-09-13 16:41:39 +0800

    Handle cross cutting concern between add data, start later and shift extremes

--- src/config/updateSeries.js
+++ src/config/updateSeries.js
@@ -4,19 +4,15 @@ import chartTypeToDataType from '../utils/chartTypeToDataType';
 import getSeriesByType from '../utils/getSeriesByType';
 import getMainSeries from '../utils/getMainSeries';
 
-export const createFutureSeries = (futureEpoch: number, lastChartData) => {
+const createFutureSeries = (futureEpoch: number, lastChartData: number[]) => {
     const futureDataPoint = [futureEpoch * 1000, lastChartData[1]];
     return seriesLine([futureDataPoint], 0, 'line', 'future', true)[0];
 };
 
-export default (chart: Chart, nextProps: any, contract: Contract) => {
-    const chartType = nextProps.type;
-
-    // by hiding series, we remove the need of chart creation
-    const dataType = chartTypeToDataType(chartType);
+const showSeriesByType = (chart, chartType) => {
     const mainTickSeries = getSeriesByType(chart, 'line');
     const mainOhlcSeries = getSeriesByType(chart, 'ohlc');
-
+    const dataType = chartTypeToDataType(chartType);
     switch (dataType) {
         case 'ticks':
             if (mainOhlcSeries) mainOhlcSeries.hide();
@@ -34,26 +30,44 @@ export default (chart: Chart, nextProps: any, contract: Contract) => {
             break;
         default: throw new Error(`Unknown data type: ${dataType}`);
     }
+};
+
+export default (chart: Chart, nextProps: any, contract: Contract) => {
+    const chartType = nextProps.type;
+    const dataType = chartTypeToDataType(chartType);
 
     const { dataMax, min, max } = chart.xAxis[0].getExtremes();
-    const dataInChart = getMainSeries(chart) ? getMainSeries(chart).options.data : [];
+    const mainSeries = getMainSeries(chart);
+    const dataInChart = mainSeries ? mainSeries.options.data : [];
     const pipSize = chart.userOptions.binary.pipSize;
+
     const nowEpoch = nowAsEpoch();
+    const contractStartLater = contract && contract.date_start > nowEpoch;
+    const prevFutureSeries = chart.get('future');
+    const hasFutureSeries = !!prevFutureSeries;
+    const prevStartLaterMillis = prevFutureSeries && prevFutureSeries.options.data[0][0];
+    const isStartLaterNewlyAdded = contractStartLater && (!hasFutureSeries || contract.date_start * 1000 !== prevStartLaterMillis);
 
+    const newDataInChartFormat = nextProps.ticks.map(dataType === 'ticks' ? tickToData : ohlcToData);
+    const oneTickDiff = doArrayDifferJustOneEntry(
+        dataInChart,
+        newDataInChartFormat,
+        (a, b) => a === b || a[0] === b[0]
+    );
+    const lastestNewData = getLast(newDataInChartFormat);
+
+    // closures
     const addNewseries = data => chart.addSeries(seriesLine(data, pipSize, chartType)[0], false);
-
     const addStartLaterData = (lastData) => {
         const futureSeries = createFutureSeries(contract.date_start, lastData);
         chart.addSeries(futureSeries, false);
     };
-
     const removeStartLaterData = () => {
         const futureSeries = chart.get('future');
         if (futureSeries) {
             futureSeries.remove(false);
         }
     };
-
     const shiftToRightWhenCloseEnough = (newDataMax: number, isCloseEnough: Boolean) => {
         if (isCloseEnough) {
             const hasNullData = dataInChart.some(d => !d[1] && d[1] !== 0);
@@ -69,40 +83,29 @@ export default (chart: Chart, nextProps: any, contract: Contract) => {
         }
     };
 
-    switch (chartType) {
-        case 'line':
-        case 'area': {
-            const newDataInChartFormat = nextProps.ticks.map(tickToData);
+    showSeriesByType(chart, chartType);
 
-            const oneTickDiff = doArrayDifferJustOneEntry(
-                dataInChart,
-                newDataInChartFormat,
-                (a, b) => a === b || a[0] === b[0]
-            );
-            const tickSeries = mainTickSeries;
-            const lastestNewData = getLast(newDataInChartFormat);
-
-            const contractStartLater = contract && contract.date_start > nowEpoch;
-
+    switch (dataType) {
+        case 'ticks': {
             if (oneTickDiff) {
                 if (!lastestNewData) {
                     return;
                 }
 
                 // add new data to existing series
-                if (tickSeries) {
-                    tickSeries.addPoint(lastestNewData, false);
+                if (mainSeries) {
+                    mainSeries.addPoint(lastestNewData, false);
                 } else {
                     addNewseries([lastestNewData]);
                 }
 
                 // handle future data
                 const newDataMax = lastestNewData[0];
-                if (contractStartLater) {
+
+                if (contractStartLater && isStartLaterNewlyAdded) {
                     addStartLaterData(lastestNewData);
                     shiftToRightWhenCloseEnough(contract.date_start * 1000, (dataMax - max) < 2000);
-                } else if (chart.get('future')){
-                    // if not start in future but contains future series
+                } else if (!contractStartLater && hasFutureSeries){
                     removeStartLaterData();
                     const mainSeriesMax = getLast(dataInChart)[0];
                     if (mainSeriesMax < max) {
@@ -111,65 +114,70 @@ export default (chart: Chart, nextProps: any, contract: Contract) => {
                         shiftToRightWhenCloseEnough(newDataMax, (dataMax - max) < 2000);
                     }
                 } else {
-                    // normal case, no future data
                     shiftToRightWhenCloseEnough(newDataMax, (dataMax - max) < 2000);
                 }
             } else {
-                if (tickSeries) {
-                    tickSeries.setData(newDataInChartFormat, false);
+                if (mainSeries) {
+                    mainSeries.setData(newDataInChartFormat, false);
                 } else {
                     addNewseries(newDataInChartFormat);
                 }
 
                 // handle future data
-                if (contractStartLater) {
+                if (contractStartLater && isStartLaterNewlyAdded) {
                     addStartLaterData(lastestNewData);
                     chart.xAxis[0].setExtremes(newDataInChartFormat[0][0], contract.date_start * 1000);
-                } else {
+                } else if (contractStartLater && hasFutureSeries) {
                     removeStartLaterData();
                 }
             }
             break;
         }
-        case 'ohlc':
-        case 'candlestick': {
-            const newDataInChartFormat = nextProps.ticks.map(ohlcToData);
-            const oneTickDiff = doArrayDifferJustOneEntry(
-                dataInChart,
-                newDataInChartFormat,
-                (a, b) => a === b || a[0] === b[0]
-            );
-
-            const ohlcSeries = mainOhlcSeries;
+        case 'candles': {
             if (oneTickDiff) {
-                const dataPoint: any = getLast(newDataInChartFormat);
-                const xData = ohlcSeries.xData;
+                const xData = mainSeries.xData;
                 const last2Epoch = xData[xData.length - 2];
                 const last3Epoch = xData[xData.length - 3];
                 const timeInterval = last2Epoch - last3Epoch;
 
-                const newDataIsWithinInterval = (dataPoint[0] - last2Epoch) <= timeInterval;
+                const newDataIsWithinInterval = (lastestNewData[0] - last2Epoch) <= timeInterval;
                 if (newDataIsWithinInterval) {
-                    dataInChart[xData.length - 1] = dataPoint;
-                } else if (ohlcSeries) {
-                    ohlcSeries.addPoint(dataPoint, false);
+                    dataInChart[xData.length - 1] = lastestNewData;
+                } else if (mainSeries) {
+                    mainSeries.addPoint(lastestNewData, false);
                 } else {
-                    addNewseries([dataPoint]);
+                    addNewseries([lastestNewData]);
                 }
 
-                const newDataMax = dataPoint[0];
-
-                // shift only happens when interval are small enough,
-                // assuming live data does not matter much when interval is huge
-                shiftToRightWhenCloseEnough(newDataMax, (dataMax - max) < 100000);
-            } else if (ohlcSeries) {
-                ohlcSeries.setData(newDataInChartFormat, false);
+                const newDataMax = lastestNewData[0];
+                if (contractStartLater && isStartLaterNewlyAdded) {
+                    addStartLaterData(lastestNewData);
+                    shiftToRightWhenCloseEnough(contract.date_start * 1000, (dataMax - max) < 100000);
+                } else if (contractStartLater && hasFutureSeries) {
+                    removeStartLaterData();
+                    const mainSeriesMax = getLast(dataInChart)[0];
+                    if (mainSeriesMax < max) {
+                        chart.xAxis[0].setExtremes(min, mainSeriesMax);
+                    } else {
+                        shiftToRightWhenCloseEnough(newDataMax, (dataMax - max) < 100000);
+                    }
+                } else {
+                    shiftToRightWhenCloseEnough(newDataMax, (dataMax - max) < 100000);
+                }
             } else {
-                addNewseries(newDataInChartFormat);
+                if (mainSeries) {
+                    mainSeries.setData(newDataInChartFormat, false);
+                } else {
+                    addNewseries(newDataInChartFormat);
+                }
+
+                if (contractStartLater && isStartLaterNewlyAdded) {
+                    addStartLaterData(lastestNewData);
+                    chart.xAxis[0].setExtremes(newDataInChartFormat[0][0], contract.date_start * 1000);
+                } else if (!contractStartLater && hasFutureSeries){
+                    removeStartLaterData();
+                }
             }
-
-            addStartLaterData(getLast(newDataInChartFormat));
-
             break;
         }
         default:
