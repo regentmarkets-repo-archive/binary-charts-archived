3b73ceb7995f7414a9614f7ac6b8e57ca04a4847
Qing Wei <qingwei@binary.com>
2016-09-23 16:20:31 +0800

    Try enable datagroup (#144)
    * Try enable datagroup
    
    * Make explicit

--- src/config/updateZones.js
+++ src/config/updateZones.js
@@ -31,7 +31,7 @@ export default (chart: Chart, newPlotLines: PlotObject[]) => {
     }
 
     const mainSeries = getMainSeries(chart);
-    if (mainSeries) {
+    if (mainSeries && zones.length > 0) {
         mainSeries.update({ zones }, false);
     }
 };
