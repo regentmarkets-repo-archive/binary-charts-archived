2c26589fb18f601d701dda258abc1687337848f6
qingwei <qingwei@binary.com>
2016-09-26 10:14:20 +0800

    Dynamic set dataGrouping

--- src/config/updateStartLater.js
+++ src/config/updateStartLater.js
@@ -23,6 +23,7 @@ export default (chart: Chart, startLaterEpoch: number, lastTick: number) => {
                 xAxis.setExtremes(min, startLaterDate);
             }
         } else {
+            getMainSeries(chart).update({ dataGrouping: { enabled: false } });
             const futureSeries = createHiddenSeries([[startLaterEpoch * 1000, lastTick], [startLaterDate, lastTick]], 'future');
             chart.addSeries(futureSeries);
             xAxis.setExtremes(min, startLaterDate);
@@ -33,6 +34,7 @@ export default (chart: Chart, startLaterEpoch: number, lastTick: number) => {
         if (oldSeries) {
             oldSeries.remove();
             const mainSeries = getMainSeries(chart);
+            mainSeries.update({ dataGrouping: { enabled: true } });
             const mainSeriesMax = mainSeries && getLast(mainSeries.options.data)[0];
 
             if (mainSeriesMax && max > mainSeriesMax) {
