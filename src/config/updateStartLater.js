95c216ea24579c5e5f182265ef595684bfb15be6
qingwei <qingwei@binary.com>
2016-09-27 11:58:15 +0800

    Make future exit time visible

--- src/config/updateStartLater.js
+++ src/config/updateStartLater.js
@@ -2,15 +2,23 @@ import { getLast } from 'binary-utils';
 import createHiddenSeries from './createHiddenSeries';
 import getMainSeries from '../utils/getMainSeries';
 
-export default (chart: Chart, startLaterEpoch: number, lastData: Object) => {
+export default (chart: Chart, contract: Object, lastData: Object) => {
+    const startEpoch = contract.date_start;
+    const expectedExitEpoch = contract.date_expiry;
+
     const xAxis = chart.xAxis[0];
     const { min, max } = xAxis.getExtremes();
 
     if (!lastData || !min || !max) return;
 
+    const startInFuture = startEpoch && startEpoch > lastData.epoch;
+    const endInFuture = expectedExitEpoch && expectedExitEpoch > lastData.epoch;
+
+    const doesInvolveFuture = startInFuture || endInFuture;
+
     const oldSeries = chart.get('future');
 
-    if (!startLaterEpoch) {
+    if (!doesInvolveFuture) {
         if (oldSeries) {
             oldSeries.remove();
             const mainSeries = getMainSeries(chart);
@@ -25,24 +33,57 @@ export default (chart: Chart, startLaterEpoch: number, lastData: Object) => {
     }
 
     const lastTick = Object.keys(lastData).length === 2 ? lastData.quote : lastData.close;
-    if (startLaterEpoch <= lastData.epoch) {
-        return;
-    }
+    const startLaterMillis = startEpoch && ((startEpoch + 5) * 1000); // 5 secs space to the right
+    const exitLaterMillis = expectedExitEpoch && ((expectedExitEpoch + 5) * 1000);
 
-    const startLaterDate = (startLaterEpoch + 5) * 1000; // 5 secs space to the right
+    if (oldSeries) {
+        const oldSeriesMax = oldSeries.options.data[1][0];
+        const seriesData = [];
 
-    if (startLaterEpoch && lastTick) {
-        if (oldSeries) {
-            const oldStartLater = Math.round(oldSeries.options.data[0][0] / 1000);
-            if (oldStartLater !== startLaterEpoch) {
-                oldSeries.setData([[startLaterEpoch * 1000, lastTick], [startLaterDate, lastTick]]);
-                xAxis.setExtremes(min, startLaterDate);
+        if (startInFuture) {
+            if (oldSeriesMax < startLaterMillis) {
+                seriesData.push([startLaterMillis, lastTick]);
             }
-        } else {
-            getMainSeries(chart).update({ dataGrouping: { enabled: false } });
-            const futureSeries = createHiddenSeries([[startLaterEpoch * 1000, lastTick], [startLaterDate, lastTick]], 'future');
-            chart.addSeries(futureSeries);
-            xAxis.setExtremes(min, startLaterDate);
         }
+
+        if (endInFuture) {
+            if (oldSeriesMax < exitLaterMillis) {
+                seriesData.push([exitLaterMillis, lastTick]);
+            }
+        }
+
+        switch (seriesData.length) {
+            case 1:
+                seriesData.push(seriesData[0]);
+                oldSeries.setData(seriesData);
+                xAxis.setExtremes(min, seriesData[1][0]);
+                break;
+            case 2:
+                oldSeries.setData(seriesData);
+                xAxis.setExtremes(min, seriesData[1][0]);
+                break;
+            default:
+                // do nothing
+        }
+    } else {
+        getMainSeries(chart).update({ dataGrouping: { enabled: false } });
+
+        const seriesData = [];
+
+        if (startInFuture) {
+            seriesData.push([startLaterMillis, lastTick]);
+        }
+
+        if (endInFuture) {
+            seriesData.push([exitLaterMillis, lastTick]);
+        }
+
+        if (seriesData.length === 1) {
+            seriesData.push(seriesData[0]);
+        }
+
+        const futureSeries = createHiddenSeries(seriesData, 'future');
+        chart.addSeries(futureSeries);
+        xAxis.setExtremes(min, startLaterMillis);
     }
 };
