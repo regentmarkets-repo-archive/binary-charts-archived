89f37cc3820ad5a38d60cd8f5d7ad499c80adb92
qingwei <qingwei@binary.com>
2016-09-27 10:24:18 +0800

    Fix bug in removing start later series

--- src/config/updateStartLater.js
+++ src/config/updateStartLater.js
@@ -3,19 +3,32 @@ import createHiddenSeries from './createHiddenSeries';
 import getMainSeries from '../utils/getMainSeries';
 
 export default (chart: Chart, startLaterEpoch: number, lastData: Object) => {
-    if (!startLaterEpoch || !lastData) return;
+    const xAxis = chart.xAxis[0];
+    const { min, max } = xAxis.getExtremes();
+
+    if (!lastData || !min || !max) return;
+
+    const oldSeries = chart.get('future');
+
+    if (!startLaterEpoch) {
+        if (oldSeries) {
+            oldSeries.remove();
+            const mainSeries = getMainSeries(chart);
+            mainSeries.update({ dataGrouping: { enabled: true } });
+            const mainSeriesMax = mainSeries && getLast(mainSeries.options.data)[0];
+
+            if (mainSeriesMax && max > mainSeriesMax) {
+                xAxis.setExtremes(min, mainSeriesMax);
+            }
+        }
+        return;
+    }
 
     const lastTick = Object.keys(lastData).length === 2 ? lastData.quote : lastData.close;
     if (startLaterEpoch <= lastData.epoch) {
         return;
     }
 
-    const xAxis = chart.xAxis[0];
-    const { min, max } = xAxis.getExtremes();
-
-    if (!min || !max) return;               // if chart is not drawn yet, abort
-
-    const oldSeries = chart.get('future');
     const startLaterDate = (startLaterEpoch + 5) * 1000; // 5 secs space to the right
 
     if (startLaterEpoch && lastTick) {
@@ -30,19 +43,6 @@ export default (chart: Chart, startLaterEpoch: number, lastData: Object) => {
             const futureSeries = createHiddenSeries([[startLaterEpoch * 1000, lastTick], [startLaterDate, lastTick]], 'future');
             chart.addSeries(futureSeries);
             xAxis.setExtremes(min, startLaterDate);
-        }
-    }
-
-    if (!startLaterEpoch) {
-        if (oldSeries) {
-            oldSeries.remove();
-            const mainSeries = getMainSeries(chart);
-            mainSeries.update({ dataGrouping: { enabled: true } });
-            const mainSeriesMax = mainSeries && getLast(mainSeries.options.data)[0];
-
-            if (mainSeriesMax && max > mainSeriesMax) {
-                xAxis.setExtremes(min, mainSeriesMax);
-            }
         }
     }
 };
