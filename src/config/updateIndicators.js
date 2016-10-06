8fe5ea1200deb9b6f18143a78c20be0eeb98dcba
qingwei <qingwei@binary.com>
2016-10-06 11:40:07 +0800

    Improve updateIndicator logic to handle removal of indicators

--- src/config/updateIndicators.js
+++ src/config/updateIndicators.js
@@ -3,21 +3,27 @@ import { exponentialMovingAverageArray } from 'binary-indicators/lib/exponential
 import { bollingerBandsArray } from 'binary-indicators/lib/bollingerBands';
 import createSeries from './createSeries';
 
-export default (chart, newData, indicatorConfs) => {
-    /**
-     * 1. init 2 series for indicator if not yet exist, named indicator0, indicator1
-     * 2. compute series for each element in indicatorConfigs
-     */
+const indicatorsSeriesPoolIds = Array.apply(null, Array(5)).map((v, i) => `indicator${i}`);
 
+export default (chart, newData, indicatorConfs) => {
     if (!newData || newData.length === 0) return;
 
     if (!chart.get('indicator0')) {
         const pipSize = chart.userOptions.binary.pipSize;
-        chart.addSeries(createSeries('indicator', 'line', [], pipSize, 'indicator0'));
-        chart.addSeries(createSeries('indicator', 'line', [], pipSize, 'indicator1'));
+        indicatorsSeriesPoolIds.forEach(id => {
+            chart.addSeries(createSeries('indicator', 'line', [], pipSize, id));
+        });
     }
 
-    indicatorConfs.forEach((conf, idx) => {
+    indicatorsSeriesPoolIds.forEach((seriesId, idx) => {
+        const conf = indicatorConfs[idx];
+        const indicatorSeries = chart.get(seriesId);
+
+        if (!conf) {
+            indicatorSeries.setData([], false);
+            return;
+        }
+
         const isOHLC = !!newData[0].open;
         const yData = isOHLC ? newData.map(d => +d.close) : newData.map(d => +d.quote);
 
@@ -25,7 +31,7 @@ export default (chart, newData, indicatorConfs) => {
 
         // todo: maybe wrap this block with try catch ?
         // or hard code contraints on high-layer?
-        switch (conf.type.toLowerCase()) {
+        switch (conf.class.toLowerCase()) {
             case 'sma':
                 indicatorYData = simpleMovingAverageArray(yData, conf);
                 break;
@@ -36,14 +42,13 @@ export default (chart, newData, indicatorConfs) => {
                 indicatorYData = bollingerBandsArray(yData, conf);
                 break;
             default:
-                // do nothing
+            // do nothing
         }
 
         const indexOffset = newData.length - indicatorYData.length;
 
         const indicatorData = indicatorYData.map((y, i) => [+newData[i + indexOffset].epoch * 1000, y]);
-        const indicatorSeries = chart.get(`indicator${idx}`);
-        indicatorSeries.update({ name: conf.name || conf.type });
-        indicatorSeries.setData(indicatorData);
+        indicatorSeries.update({ name: conf.name || conf.type }, false);
+        indicatorSeries.setData(indicatorData, false);
     });
 };
